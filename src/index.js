ae16b95be9bcdaf7d15a154dc1c3464580b82b61
Salinger2333 <1565664814@qq.com>
2025-12-01 19:50:11 +0800

    重构代码以添加新任务和项目的功能，优化用户交互feature: add the ability to add todos and project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { ProjectManager } from "./logic/ProjectManager";
 import { sidebar } from "./dom/sidebar.js";
 import { board } from "./dom/board.js";
+import { initEditModal } from "./dom/edit-handler.js";
+import './style.css'
+
 function initTestData() {
     // 默认项目肯定在 index 0
-    
+
     ProjectManager.addProject("default");
     const defaultProject = ProjectManager.getAllProjects()[0];
     defaultProject.addTodo({
@@ -29,6 +32,53 @@ function initTestData() {
         priority: "high"
     });
 }
-initTestData();
+
+const dialog = document.querySelector('dialog');
+
+// add new todo
+const addTodoBtn = document.querySelector('#addTodoBtn');
+const subBtn = document.querySelector('#subBtn');
+const cancelBtn = document.querySelector('#cancelBtn');
+addTodoBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    dialog.showModal()
+})
+cancelBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    dialog.close()
+})
+subBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    const title = document.querySelector('#title').value
+    const description = document.querySelector('#description').value
+    const dueDate = document.querySelector('#date').value
+    const priority = document.querySelector('#priority').value
+    const curProject = ProjectManager.getCurProject()
+    curProject.addTodo({
+        title,
+        description,
+        dueDate,
+        priority
+    })
+    dialog.close()
+    board()
+})
+
+
+// add new project
+const addProjectBtn = document.querySelector('#addProjectBtn');
+addProjectBtn.addEventListener('click', () => {
+    const projectName = prompt("请输入新项目的名称:");
+    if (projectName) {
+        ProjectManager.addProject(projectName);
+    }
+    sidebar()
+})
+
+
+
+
+initTestData()
 sidebar();
+initEditModal();
 board()
