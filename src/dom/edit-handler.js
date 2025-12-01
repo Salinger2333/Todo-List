147cae0a4fb66f322c603617b0dff94e0b8a9314
Salinger2333 <1565664814@qq.com>
2025-12-01 19:47:12 +0800

    add edit feature

---
+++ src/dom/edit-handler.js
@@ -0,0 +1,47 @@
+import { ProjectManager } from "../logic/ProjectManager.js";
+import { board } from "./board.js";
+
+const editDialog = document.getElementById('editDialog');
+const editTitle = document.getElementById('edit-title');
+const editDescription = document.getElementById('edit-description');
+const editDate = document.getElementById('edit-date');
+const editPriority = document.getElementById('edit-priority');
+const saveBtn = document.getElementById('edit-subBtn');
+const cancelBtn = document.getElementById('edit-cancelBtn');
+
+function initEditModal() {
+    cancelBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        editDialog.close();
+    });
+
+    saveBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const uid = editDialog.dataset.editingUid;
+        if (!uid) return;
+
+        const newDetail = {
+            title: editTitle.value,
+            description: editDescription.value,
+            dueDate: editDate.value,
+            priority: editPriority.value
+        };
+
+        ProjectManager.getCurProject().setTodo(uid, newDetail);
+        board();
+        editDialog.close();
+    });
+}
+
+function openEditModal(todo) {
+    editTitle.value = todo.title;
+    editDescription.value = todo.description;
+    editDate.value = todo.dueDate;
+    editPriority.value = todo.priority;
+    
+    // Store the UID on the dialog element itself to retrieve it later
+    editDialog.dataset.editingUid = todo.uid;
+    editDialog.showModal();
+}
+
+export { initEditModal, openEditModal };
--- src/dom/todo-item.js
+++ src/dom/todo-item.js
@@ -1,3 +1,7 @@
+import { ProjectManager } from "../logic/ProjectManager"
+import { board } from "./board"
+import { openEditModal } from "./edit-handler.js"
+
 function createTodoItem(todo) {
     const div = document.createElement('div')
     const titleTodo = document.createElement('div')
@@ -5,6 +9,8 @@ function createTodoItem(todo) {
     const toggleTodo = document.createElement('div')
     const priorityTodo = document.createElement('div')
     const descriptionTodo = document.createElement('div')
+    const editTodo = document.createElement('button')
+    const deleteTodo = document.createElement('button')
 
     div.classList.add('todo-item')
     div.dataset.uid = todo.uid
@@ -13,18 +19,29 @@ function createTodoItem(todo) {
     titleTodo.textContent = todo.title
     dueDateTodo.classList.add('todo-dueDate')
     dueDateTodo.textContent = todo.dueDate
-    toggleTodo.classList.add('todo-toggle')
+    // toggleTodo.classList.add('todo-toggle')
     priorityTodo.classList.add('todo-priority')
     priorityTodo.textContent = todo.priority
     descriptionTodo.classList.add('todo-description')
     descriptionTodo.textContent = todo.description
-    div.append(titleTodo,descriptionTodo, dueDateTodo, priorityTodo, toggleTodo)
+    editTodo.classList.add('todo-edit')
+    editTodo.textContent = '编辑'
+    deleteTodo.classList.add('todo-delete')
+    deleteTodo.textContent = '删除'
+    div.append(titleTodo, descriptionTodo, dueDateTodo, priorityTodo, toggleTodo, editTodo, deleteTodo)
+
+    editTodo.addEventListener('click', (event) => {
+        event.preventDefault()
+        openEditModal(todo)
+    })
+    deleteTodo.addEventListener('click', (event) => {
+        event.preventDefault()
+        const curProject = ProjectManager.getCurProject()
+        curProject.removeTodo(todo.uid)
+        board()
+    })
     return div
 
-    // const expandTodo = function(){
 
-    // }
-
-    // div.addEventListener('click', expandTodo())
 }
 export { createTodoItem }
